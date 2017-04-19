df0d9a4dc367c8e1af9eab4be24d2d7ee271d586
casual-observer <raghavan.anisha@gmail.com>
2017-04-19 18:07:44 +0530

    v2 created: test

--- app/src/main/java/com/example/anisha/trialdatabase/MainActivity.java
+++ app/src/main/java/com/example/anisha/trialdatabase/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.anisha.trialdatabase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        TextView console = (TextView) findViewById(R.id.textView);
         myDB = new DatabaseHelper(this);
-
+        console.append("\n\nTABLE NAMES v2\n");
+//        for(String l: myDB.getTables()) {
+//            console.append(l + "\n");
+//        }
     }
 }
