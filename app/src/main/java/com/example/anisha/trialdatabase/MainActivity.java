c8426d9f97638c5f0ceb744c982f031907cd2eb6
TheChesireCat <aliceoxenbury@gmail.com>
2017-04-20 16:02:15 +0530

    Changed stuff

--- app/src/main/java/com/example/anisha/trialdatabase/MainActivity.java
+++ app/src/main/java/com/example/anisha/trialdatabase/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.anisha.trialdatabase;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +18,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myDB = new DatabaseHelper(this);
+        TextView console = (TextView) findViewById(R.id.textView);
+        console.append("\n\nLoading...");
 
+        console.append("\n\n1. TABLE NAMES ARE\n\n");
+        for(String l : myDB.getTables()){
+            console.append(l+"\n");
+        }
+        SQLiteDatabase db = myDB.getWritableDatabase();
+        db.execSQL("CREATE TABLE User_Info2 (USERID INTEGER PRIMARY KEY, USERNAME TEXT, PASSWORD TEXT,NAME TEXT, PHONENO TEXT)");
+        db.execSQL("CREATE TABLE User_Info3 (USERID INTEGER PRIMARY KEY, USERNAME TEXT, PASSWORD TEXT,NAME TEXT, PHONENO TEXT)");
+        console.append("\n\n2. TABLE NAMES ARE\n\n");
+        for(String l : myDB.getTables()){
+            console.append(l+"\n");
+        }
     }
 }
